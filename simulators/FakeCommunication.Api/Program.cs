fd8a50adb3da131f15bc33f77e37bd011bbdbd59
Huseyin Kayakiran <kayakiranh@gmail.com>
2026-08-09 21:49:04 +0300

    add swagger and security baseline to fake communication

--- simulators/FakeCommunication.Api/Program.cs
+++ simulators/FakeCommunication.Api/Program.cs
@@ -1,13 +1,16 @@
 using System.Collections.Concurrent;
 using FakeCommunication.Api.Models;
+using FinWallet.Shared.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddFinWalletWebPlatform("FakeCommunication.Api");
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ConcurrentDictionary<Guid, FakeMessageRecord>>();
 
 var app = builder.Build();
 
+app.UseFinWalletWebPlatform();
 app.MapControllers();
 
 app.Run();
