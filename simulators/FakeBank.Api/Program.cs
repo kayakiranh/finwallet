876baacf5aca8644c0b02820a2fadd691c3a6828
Huseyin Kayakiran <kayakiranh@gmail.com>
2026-08-09 17:44:14 +0300

    refactor: use controller Web API in fake bank

--- simulators/FakeBank.Api/Program.cs
+++ simulators/FakeBank.Api/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/health/live", () => Results.Ok(new { status = "ok", service = "FakeBank.Api" }));
+app.MapControllers();
 
 app.Run();
